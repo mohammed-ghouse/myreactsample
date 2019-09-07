2db303b494f70d48e7c6aadabe4fd670f81b1abb
shaikMohammedGhouse <ghouse.s@indivillage.com>
2019-09-07 17:48:07 +0530

    Updates

---
+++ precache-manifest.440689f6bd49f36e1190e1c27c764a01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b519ffc1a75c61567cc97f8799dcef38",
+    "url": "/myreactsample/index.html"
+  },
+  {
+    "revision": "a21e4a1763731c955da4",
+    "url": "/myreactsample/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fcf3101032b7819f2a94",
+    "url": "/myreactsample/static/js/2.a635e644.chunk.js"
+  },
+  {
+    "revision": "a21e4a1763731c955da4",
+    "url": "/myreactsample/static/js/main.cded6439.chunk.js"
+  },
+  {
+    "revision": "ad0ce7247c29481cec29",
+    "url": "/myreactsample/static/js/runtime~main.f97fe9de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreactsample/static/media/logo.5d5d9eef.svg"
+  }
+]);
